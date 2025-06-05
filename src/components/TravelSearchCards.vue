f1a489c4849a9f607e447e017fe6308469682b8c
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-06-05 14:19:33 +1000

    added no search term

--- src/components/TravelSearchCards.vue
+++ src/components/TravelSearchCards.vue
@@ -1,33 +1,36 @@
 <script setup lang="ts">
-    import type { Destination } from './../types/Destination';
-    import { computed } from 'vue';
+import type { Destination } from './../types/Destination';
+import { computed } from 'vue';
 
-    const { destinations, loading, error } = defineProps<{
-        destinations: Destination[];
-        loading: boolean;
-        error: string | null;
-    }>();
+const { destinations, loading, error } = defineProps<{
+    destinations: Destination[];
+    loading: boolean;
+    error: string | null;
+}>();
 
-    const sortedDestinations = computed(() => {
-        return [...destinations].sort((a, b) => a.id - b.id);
-    });
+const sortedDestinations = computed(() => {
+    return [...destinations].sort((a, b) => a.id - b.id);
+});
 
-    console.log({destinations})
+console.log({ destinations })
 
-  
+
 </script>
 
 <template>
-     <div v-if="loading">Loading...</div>
+    <p v-if="loading">Loading...</p>
 
-     <div v-if="error">Uh oh, there was an error: {{ error }}</div>
+    <p v-if="error">Uh oh, there was an error: {{ error }}</p>
 
-     <div v-else>
+    <p v-if="destinations.length < 1">
+        We couldn't find any images that matched your search term.
+    </p>
 
+    <div v-else>
         <li v-for="desti in sortedDestinations" :key="desti.id">
-        {{ desti.id }} ({{ desti.alt }})
-      </li>
-     </div>
+            {{ desti.id }} ({{ desti.alt }})
+        </li>
+    </div>
 
     <article class="card text-bg-dark">
         <img src="..." class="card-img" alt="...">
